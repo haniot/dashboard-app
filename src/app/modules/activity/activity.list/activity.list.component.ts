1013876d6b7e5160af86c16a92388db26d5941c9
Adalcino Junior <adaljunior.inf@gmail.com>
2020-01-16 17:32:27 -0300

    [FT12] - Integração Fitbit User Stories: Nemhuma. Bugs: Nenhum. Outras Informações: Adicionando página para listagem das atividades físicas.

--- src/app/modules/activity/activity.list/activity.list.component.ts
+++ src/app/modules/activity/activity.list/activity.list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TimeSeriesType } from '../models/time.series'
+import { ActivatedRoute } from '@angular/router'
+import { PhysicalActivity } from '../models/physical.activity'
 
 @Component({
     selector: 'app-activity.list',
@@ -6,11 +9,57 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./activity.list.component.scss']
 })
 export class ActivityListComponent implements OnInit {
+    data: PhysicalActivity[];
+    patientId: string;
+    measurementSelected: TimeSeriesType;
+    timeSeriesTypes: any;
+    currentDate: Date;
+    selectAll: boolean;
+    stateButtonRemoveSelected: boolean;
+    showSpinner: boolean;
 
-    constructor() {
+    constructor(private activeRouter: ActivatedRoute) {
+        this.data = [];
+        this.timeSeriesTypes = [TimeSeriesType.steps, TimeSeriesType.calories, TimeSeriesType.distance];
+        this.measurementSelected = TimeSeriesType.steps;
+        this.currentDate = new Date();
     }
 
     ngOnInit() {
+        this.activeRouter.paramMap.subscribe((params) => {
+            this.patientId = params.get('patientId');
+        })
+    }
+
+    applyFilter($event): void {
+
+    }
+
+    previosDay(): void {
+        this.currentDate = new Date(this.currentDate.getTime() - (24 * 60 * 60 * 1000));
+    }
+
+    nextDay(): void {
+        if (!this.isToday(this.currentDate)) {
+            this.currentDate = new Date(this.currentDate.getTime() + (24 * 60 * 60 * 1000));
+        }
+    }
+
+    isToday(date: Date): boolean {
+        const today = new Date();
+        return date.toISOString().split('T')[0] === today.toISOString().split('T')[0];
+    }
+
+    selectAllActivities(): void {
+
+    }
+
+    removeSelected(): void {
+
+    }
+
+    trackById(index, item: PhysicalActivity) {
+        return item.id
     }
 
 }
